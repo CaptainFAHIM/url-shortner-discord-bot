e0b9df8ee1eff1cd989d264db2f71e1c7af1d900
CaptainFAHIM <mehedihfahim12@gmail.com>
2023-10-24 11:30:20 +0600

    Added new api

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const {Client, GatewayIntentBits} = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 const axios = require("axios");
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
-    partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
+  partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"]
 });
 
 // Variables
@@ -11,28 +11,41 @@ const client = new Client({
 const prefix = "/";
 
 client.on("ready", (client) => {
-    console.log(`Url-shortner is online ${client.user.tag}`);
+  console.log(`Url-shortner is online ${client.user.tag}`);
 });
 client.on("messageCreate", async (message) => {
-    if(!message.author.bot){
-        if(message.content.toLowerCase().startsWith(`${prefix}cshort`)){
-            const args = message.content.split(" ");
-            if (args.length !== 2) {
-                message.channel.send('Please provide a valid YouTube URL.');
-                return;
-              }
-              const url = args[1];
-              try {
-                const response = await axios.post(`https://ulvis.net/API/write/get?url=${url}&private=1`);
-                message.reply(response.data.data.url);
-              } catch (error) {
-                console.log(error.message);
-              }
-            
-        }
-        
+  if (!message.author.bot) {
+    if (message.content.toLowerCase().startsWith(`${prefix}cshort`)) {
+      const args = message.content.split(" ");
+      if (args.length !== 2) {
+        message.channel.send('Please provide a valid URL.');
+        return;
+      }
+      const url = args[1];
+
+        var myHeaders = new Headers();
+        myHeaders.append("apikey", process.env.API_KEY);
+
+        var raw = url;
+
+        var requestOptions = {
+          method: 'POST',
+          redirect: 'follow',
+          headers: myHeaders,
+          body: raw
+        };
+
+        fetch("https://api.apilayer.com/short_url/hash", requestOptions)
+          .then(response => response.json())
+          .then(result => {
+            message.reply(result.short_url);
+          })
+          .catch(error => console.log('error', error));
+      
     }
-    
+
+  }
+
 });
 
 client.login(process.env.TOKEN);
